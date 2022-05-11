7b8f70c0c1ad5d74958ab01009fc775943b79020
sxm <xiaoming9102020@outlook.com>
2022-05-11 12:16:04 +0800

    initial commit

---
+++ src/wordCount.java
@@ -0,0 +1,75 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * @author smh
+ */
+public class wordCount {
+    public static void main(String[] args) {
+        File file = new File(args[1]);
+        try {
+            if (args[0].equals("-c")) {
+                System.out.println(countCharacters(file));
+            } else if (args[0].equals("-w")) {
+                System.out.println(countWords(file));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 统计文件中所有字符的个数，包括换行符
+     * @param file 传入的文件对象
+     * @return 统计的字符数
+     */
+    public static int countCharacters(File file) throws IOException {
+        FileReader reader = new FileReader(file.getCanonicalPath());
+        char[] data = new char[50];
+        int readCount;
+        int characterCount = 0;
+        while ((readCount = reader.read(data)) != -1) {
+            characterCount += readCount;
+        }
+        reader.close();
+        return characterCount;
+    }
+
+    /**
+     * 统计文件中所有单词的个数，但是不做单词的合法性检查
+     * @param file 传入的文件对象
+     * @return 统计的单词数
+     */
+    public static int countWords(File file) throws IOException {
+        FileReader reader = new FileReader(file.getCanonicalPath());
+        char[] data = new char[50];
+        int readCount;
+        int wordCount = 0;
+        StringBuffer buffer = new StringBuffer();
+        while ((readCount = reader.read(data)) != -1) {
+            for (int i = 0; i < readCount - 1; i++) {
+                if (!isCharacterContained(data[i]) && isCharacterContained(data[i + 1])) {
+                    wordCount ++;
+                }
+            }
+        }
+        reader.close();
+        return wordCount;
+    }
+
+    /**
+     * 判断传入的字符是否为指定的字符
+     * @param c 待检测的字符
+     * @return true 则表明参数c是指定的字符， false 则相反
+     */
+    public static boolean isCharacterContained(char c) {
+        char[] punctuationMarks = {',', '.', ';', '?', '"', ':', ' '};
+        for (Character character : punctuationMarks) {
+            if (c == character) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
